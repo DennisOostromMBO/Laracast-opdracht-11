f2c43fad21500d2478bacad1d0d7a9cd17622cb2
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-09-13 14:22:06 +0200

    ik ben bij begin les 5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About page</title>
-</head>
-<body>
-<nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
-    <h1>Hello from the home page</h1>
-</body>
-</html>
+<x-layout >
+    <x-slot:heading>
+       Home Page
+</x-slot:heading>
+   <h1>Hello from the home page</h1>
+</x-layout>
