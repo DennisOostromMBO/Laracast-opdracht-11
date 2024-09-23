f87eef003bcc56652a62499f5b285a7a9299159a
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-09-23 11:36:35 +0200

    les 9 af

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,38 +1,11 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr; 
 
-class Job {
-    public static function all(): array
-     {
-        return [
-            [
-               'id' => 1,
-               'title' => 'Director',
-               'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];    
-     }
-     
-     public static function find(int $id): array
-    {
-       $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+use Illuminate\Database\Eloquent\Model;
 
-       if (! $job) {
-        abort(484);
-       }
+class Job extends Model {
+   protected $table = 'job_listings';
 
-       return $job;
-    }
-}
+   protected $fillable = ['title', 'salary'];
+  }
